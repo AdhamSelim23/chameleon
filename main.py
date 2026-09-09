e48f5593914ee85eb54470a50e827246fdb10f77
adham <adhamselim.uni@gmail.com>
2026-09-09 17:16:35 +0300

    Basic file upload+download

--- main.py
+++ main.py
@@ -15,14 +15,21 @@ VIDEO_DIR = Path("testing")
 async def root():
     return{"message": "Hellow"}
 
-@app.post("/videoCompress")
+@app.post("/videoCompress",
+        response_class=FileResponse,
+        responses={
+        200: {
+            "content": {"video/mp4": {}}  
+        }
+    })
 async def upload_file(file: UploadFile):
     data = file.file
     async with aiofiles.open(VIDEO_DIR / 'input.mp4', 'wb') as out_file:
         while content := await file.read(1024):  # async read chunk
             await out_file.write(content)  # async write chunk
     compress_video(VIDEO_DIR / 'input.mp4', str(VIDEO_DIR / 'output.mp4'))
-    return FileResponse(str(VIDEO_DIR / 'output.mp4'))
+    file_path = str(VIDEO_DIR / 'output.mp4')
+    return FileResponse(file_path, media_type="vide/mp4", filename="output.mp4" )
 
 
 if __name__ == '__main__':

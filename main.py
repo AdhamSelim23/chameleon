3a316791af74c90559f9310e6cd48ed0e701ab0b
adham <adhamselim.uni@gmail.com>
2026-09-09 17:02:19 +0300

    Basic compression function + fastapi skeleton
    File response still broken

---
+++ main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import FileResponse
+import uvicorn
+from vcompression.compress import compress_video
+import os
+from pathlib import Path
+import aiofiles
+
+app = FastAPI()
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+VIDEO_DIR = Path("testing")
+
+@app.get("/")
+async def root():
+    return{"message": "Hellow"}
+
+@app.post("/videoCompress")
+async def upload_file(file: UploadFile):
+    data = file.file
+    async with aiofiles.open(VIDEO_DIR / 'input.mp4', 'wb') as out_file:
+        while content := await file.read(1024):  # async read chunk
+            await out_file.write(content)  # async write chunk
+    compress_video(VIDEO_DIR / 'input.mp4', str(VIDEO_DIR / 'output.mp4'))
+    return FileResponse(str(VIDEO_DIR / 'output.mp4'))
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)

c779243a648032cfb318a21010059e2583e00237
MREnriza <muhammadreynardenriza@student.utwente.nl>
2026-09-11 10:26:43 +0200

    new compress button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,8 @@
 import ImportBtn from './components/ImportBtn.tsx'
+import CompressBtn from './components/CompressBtn.tsx'
 
 function App() {
-  return <div><ImportBtn /></div>
+  return <div><ImportBtn /><CompressBtn /></div>
 }
 
 export default App
---
+++ frontend/src/components/CompressBtn.tsx
@@ -0,0 +1,11 @@
+function CompressBtn() {
+  return (
+    <div>
+      <button type="button" className="btn btn-secondary">
+        Compress
+      </button>
+    </div>
+  );
+}
+
+export default CompressBtn;

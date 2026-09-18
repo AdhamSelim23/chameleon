d69a8e6fa87d498fa3c0e33d15cd8a958df6cb51
MREnriza <muhammadreynardenriza@student.utwente.nl>
2026-09-18 09:53:06 +0200

    basic css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,34 @@
-import { useState } from 'react'
-import Compress from './components/Compress.tsx'
-import Download from './components/Download.tsx'
-import Merge from './components/Merge.tsx'
-import Navbar, { type Tool } from './components/Navbar.tsx'
+import { useState } from "react";
+import Compress from "./components/Compress.tsx";
+import Download from "./components/Download.tsx";
+import Merge from "./components/Merge.tsx";
+import Navbar, { type Tool } from "./components/Navbar.tsx";
 
 function App() {
-  const [activeTool, setActiveTool] = useState<Tool>('compress')
+  const [activeTool, setActiveTool] = useState<Tool>("compress");
 
   return (
-    <main>
-      <Navbar activeTool={activeTool} onToolChange={setActiveTool} />
-      {activeTool === 'compress' && <Compress />}
-      {activeTool === 'download' && <Download />}
-      {activeTool === 'merge' && <Merge />}
-    </main>
-  )
+    <div className="app-frame">
+      <div className="page-frame">
+        <header className="top-bar">
+          <div className="brand-title">
+            <div>
+              <h1>Chameleon</h1>
+            </div>
+          </div>
+        </header>
+
+        <main className="app-body">
+          <Navbar activeTool={activeTool} onToolChange={setActiveTool} />
+          <section className="tool-panel">
+            {activeTool === "compress" && <Compress />}
+            {activeTool === "download" && <Download />}
+            {activeTool === "merge" && <Merge />}
+          </section>
+        </main>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
